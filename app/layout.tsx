b159356a61a757fb79ee7adf7992c47543c5dcc3
yuya <kinjo.bobchin@gmail.com>
2024-04-30 09:50:28 +0900

    グローバルCSSとTailwindの設定を更新しました

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
-import "@/globals.css";
+import "@/app/globals.css";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +19,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
