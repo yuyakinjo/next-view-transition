1cd492dd1ee43576c91fdf533738d10bfb5a57f3
yuya <kinjo.bobchin@gmail.com>
2024-08-10 01:48:43 +0900

    refactor: Extract Favorite Monsters list to separate component
    The code for displaying the Favorite Monsters list has been extracted from the Dashboard component into a new, separate component called FavoriteList. This change improves modularity and readability of the code by separating concerns. The FavoriteList component now takes in a 'monsters' prop and handles rendering of favorite monsters.

---
+++ components/favorite-list.tsx
@@ -0,0 +1,22 @@
+import type { Monster } from "@/schemas/monster";
+import { MonsterCard } from "./monster-card";
+
+interface Props {
+  monsters: Monster[];
+}
+
+export const FavoriteList = ({ monsters }: Props) => {
+  if (!monsters.length) return null;
+  return (
+    <div className="col-span-1 md:col-span-2 lg:col-span-3">
+      <h2 className="text-xl font-bold mb-4">Favorite Monsters</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {monsters
+          .filter((monster) => monster.favorite)
+          .map((monster) => (
+            <MonsterCard monster={monster} key={monster.id} />
+          ))}
+      </div>
+    </div>
+  );
+};
